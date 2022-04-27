8c2960ea2c6f611a8ea40a96a0fa00d2ee5b2ae0
Lucas Sousa <lucas@hibrido.com.br>
2022-04-26 23:19:08 -0300

    Feat: utilizein uma lib react-modal para começar a desenvolver o modal no projeto.

--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import logoImg from '../../assets/img/Logo.svg';
+import Modal from 'react-modal'; 
 
 import { Container, Content } from './style';
 
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false); 
+     
+    function handleOpenNewTransactionModal(){
+
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false)
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt  money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     nova transação
                 </button>
            </Content>
