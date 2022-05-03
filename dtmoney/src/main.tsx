e6987afff7b02f7010e0629ca0bf141f0bc3ab9b
Lucas Sousa <lucas@hibrido.com.br>
2022-05-02 21:07:46 -0300

    Feat: Criou um banco de dados schema

--- dtmoney/src/main.tsx
+++ dtmoney/src/main.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createServer, Model} from 'miragejs'; 
+import {createServer, Model, Server} from 'miragejs'; 
 import {App}from './App'
 import Schema from 'miragejs/orm/schema';
 
 createServer({
   models: {
     transaction: Model, 
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions:[
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+
+        {
+          id: 2,
+          title: "Freelance de website",
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        }
+      ],
+    })
   },
 
   routes(){
