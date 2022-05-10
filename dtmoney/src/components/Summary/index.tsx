7874e84575af50a54c50856a4f3d0bc9b869a5bd
Lucas Sousa <lucas@hibrido.com.br>
2022-05-10 13:27:41 -0300

    Feat: utilizando a função reduce para obter o valor totla de retirada e saída

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -1,33 +1,67 @@
-import { useContext } from 'react';
-import incomeImg from '../../assets/img/Entradas.svg'; 
-import Saídas from '../../assets/img/Saídas.svg'; 
-import Total from '../../assets/img/Total.svg'; 
-import { TransactionsContext } from '../../TransactionsContext';
+
+import incomeImg from '../../assets/img/Entradas.svg';
+import Saídas from '../../assets/img/Saídas.svg';
+import Total from '../../assets/img/Total.svg';
+import { useTransactions } from '../../hooks/useTransactions';
+
 
 import { Container } from "./styles";
 
 
-export function Summary(){
-    const {transactions} = useContext(TransactionsContext)
+export function Summary() {
+    const { transactions } = useTransactions(); 
+
+    /* const totalDeposits = transactions.reduce((acc, transaction) =>{
+          if(transaction.type == 'deposit'){
+              return acc + transaction.amount; 
+          }
+  
+          return acc;  
+      }, 0)*/
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type == 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0
+    })
+
     return (
         <Container>
             <div>
                 <header>
                     <p>Entradas</p>
-                    <img src={incomeImg}/>
+                    <img src={incomeImg} />
                 </header>
                 <strong>
-                    R$ 1000,00
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+
                 </strong>
             </div>
 
             <div>
                 <header>
                     <p>Saídas</p>
-                    <img src={Saídas} alt="Saídas"/>
+                    <img src={Saídas} alt="Saídas" />
                 </header>
                 <strong>
-                  -R$ 500,00
+                    -
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdraws)}
                 </strong>
             </div>
 
@@ -35,10 +69,13 @@ export function Summary(){
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
-                  <img src={Total} alt="Total"/>
+                    <img src={Total} alt="Total" />
                 </header>
                 <strong>
-                    R$ 500,00
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
                 </strong>
             </div>
         </Container>
